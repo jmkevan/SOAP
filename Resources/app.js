94d73272a1d20e1a3104ba976b4006363379169c
Alex Cam Liu <alexcamliu@dhcp-128-171-42-192.nursing.hawaii.edu>
2013-06-20 15:15:49 -1000

    android stuff

--- Resources/app.js
+++ Resources/app.js
@@ -1,4 +1,22 @@
-//Get the object from the ApplicationWindow.js to create the main UI
-var MainWindow = require('ui/common/MainWindow');
-//Call the method exported
-var Window = new MainWindow ();
+/**
+ * Create a Navigation controller depending on which os using.
+ */
+
+//Create the Navigation Controller for android
+var NavigationController = require ("ui/common/NavigationController").NavigationController;
+var controller = new NavigationController ();
+
+//if Android
+if(Ti.Platform.osname === 'android') {
+	//create the main UI
+	var AndroidUI = require ('ui/android/MainWindow');
+	//open the main UI with the nav controller
+	controller.open(new AndroidUI (controller));
+}
+else if (Ti.Platform.osname === 'iphone') {
+	//create the main UI
+	var MainWindow = require('ui/iphone/MainWindow');
+	//Call the method exported
+	var Window = new MainWindow ();
+}	
+
